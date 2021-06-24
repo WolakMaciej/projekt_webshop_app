137ec46e51481ca3d67fe1cb75cbb497718083a7
MaciejWolak <maciejwolak@gmail.com>
2021-06-24 20:35:04 +0200

    add table name

--- src/main/java/com/example/webshopapp/model/User.java
+++ src/main/java/com/example/webshopapp/model/User.java
@@ -13,6 +13,7 @@ import java.util.Collections;
 
 @Data
 @Entity
+@Table(name="`users`")
 public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
