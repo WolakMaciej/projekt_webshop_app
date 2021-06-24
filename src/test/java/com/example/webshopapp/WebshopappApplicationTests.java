14d6b3ad594c9edab74ce277f30ca833b27204dc
MaciejWolak <maciejwolak@gmail.com>
2021-06-24 16:21:19 +0200

    add tests

---
+++ src/test/java/com/example/webshopapp/WebshopappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webshopapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebshopappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
