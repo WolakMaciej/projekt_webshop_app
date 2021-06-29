75264139e7ea1778967763c162811e5d30a687fd
Maciej Wolak <74461485+WolakMaciej@users.noreply.github.com>
2021-06-29 10:31:32 +0200

    Update ProductController.java

--- src/main/java/com/example/webshopapp/controller/ProductController.java
+++ src/main/java/com/example/webshopapp/controller/ProductController.java
@@ -14,7 +14,7 @@ import javax.validation.Valid;
 import java.io.IOException;
 import java.util.List;
 
-@CrossOrigin("http://localhost:4200")
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/")
 public class ProductController {
