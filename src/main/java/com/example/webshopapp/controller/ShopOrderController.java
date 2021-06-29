88581de5d2b358886cbb52cafac568e857ba7c48
Maciej Wolak <74461485+WolakMaciej@users.noreply.github.com>
2021-06-29 10:32:54 +0200

    Update ShopOrderController.java

--- src/main/java/com/example/webshopapp/controller/ShopOrderController.java
+++ src/main/java/com/example/webshopapp/controller/ShopOrderController.java
@@ -18,7 +18,7 @@ import javax.persistence.EntityNotFoundException;
 import javax.validation.Valid;
 import java.util.List;
 
-@CrossOrigin("http://localhost:4200")
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/")
 public class ShopOrderController {
