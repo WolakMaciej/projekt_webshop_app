69f4e6d15f1d899bda5467e0689ea1c3b40912aa
Maciej Wolak <74461485+WolakMaciej@users.noreply.github.com>
2021-06-29 10:41:05 +0200

    Update ProductController.java
    https://webshopangular2.herokuapp.com/

--- src/main/java/com/example/webshopapp/controller/ProductController.java
+++ src/main/java/com/example/webshopapp/controller/ProductController.java
@@ -14,7 +14,7 @@ import javax.validation.Valid;
 import java.io.IOException;
 import java.util.List;
 
-@CrossOrigin("*")
+@CrossOrigin("https://webshopangular2.herokuapp.com")
 @RestController
 @RequestMapping("/")
 public class ProductController {
